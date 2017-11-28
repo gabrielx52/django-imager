7a5fd5bd5005df0aec1d49c053be8c02408ad8ee
gabrielx52 <gabriel.meringolo@gmail.com>
2017-11-28 10:00:31 -0800

    Push for demo.

--- imagersite/imager_images/models.py
+++ imagersite/imager_images/models.py
@@ -4,34 +4,39 @@ from django.db import models
 from imager_profile.models import ImagerProfile
 
 
-class Photo(models.Model):
+class ImageBaseClass(models.Model):
+    """Base class for Photo and Album classes."""
+
+    PRIVATE = 'PRVT'
+    SHARED = 'SHRD'
+    PUBLIC = 'PBLC'
+
+    PUBLISHED = ((PRIVATE, 'private'),
+                 (SHARED, 'shared'),
+                 (PUBLIC, 'public'))
+    user = models.ForeignKey(ImagerProfile, on_delete=models.CASCADE)
+    title = models.CharField(max_length=180)
+    description = models.CharField(max_length=500, blank=True, null=True)
+    date_modified = models.DateField(auto_now=True)
+    date_published = models.DateField(blank=True, null=True)
+    published = models.CharField(choices=PUBLISHED, max_length=8)
+
+    class Meta:
+        """Meta."""
+
+        abstract = True
+
+
+class Photo(ImageBaseClass):
     """Photo model."""
 
-    PUBLISHED = (('PRVT', 'private'),
-                 ('SHRD', 'shared'),
-                 ('PBLC', 'public'))
     image = models.ImageField(upload_to='images')
-    user = models.ForeignKey(ImagerProfile, on_delete=models.CASCADE)
-    title = models.CharField(max_length=180)
-    description = models.CharField(max_length=500, blank=True, null=True)
     date_uploaded = models.DateField(editable=False, auto_now_add=True)
-    date_modified = models.DateField(auto_now=True)
-    date_published = models.DateField(blank=True, null=True)
-    published = models.CharField(choices=PUBLISHED, max_length=8)
 
 
-class Album(models.Model):
+class Album(ImageBaseClass):
     """Album model."""
 
-    PUBLISHED = (('PRVT', 'private'),
-                 ('SHRD', 'shared'),
-                 ('PBLC', 'public'))
-    user = models.ForeignKey(ImagerProfile, on_delete=models.CASCADE)
     photo = models.ManyToManyField(Photo, related_name='album')
-    title = models.CharField(max_length=180)
-    description = models.CharField(max_length=500, blank=True, null=True)
     cover = models.ImageField(upload_to='images')
     date_created = models.DateField(editable=False, auto_now_add=True)
-    date_modified = models.DateField(auto_now=True)
-    date_published = models.DateField(blank=True, null=True)
-    published = models.CharField(choices=PUBLISHED, max_length=8)
