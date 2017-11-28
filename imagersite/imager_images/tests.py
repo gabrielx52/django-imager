e23dbe43c819795729039e07c7bc460ae60b40a8
gabrielx52 <gabriel.meringolo@gmail.com>
2017-11-27 17:07:25 -0800

    Added more tests.

--- imagersite/imager_images/tests.py
+++ imagersite/imager_images/tests.py
@@ -31,10 +31,12 @@ class PhotoTestCase(TestCase):
         j_profile = jimbo.profile
         j_profile.location = "Buffalo"
         j_profile.save()
-        # for i in range(30):
-        #     self.photo = PhotoFactory.create()
-        #     self.photo.user = j_profile
-        #     self.photo.save()
+        album = Album(user=j_profile, title='The Album')
+        album.save()
+        for i in range(30):
+            photo = Photo(user=j_profile, title=f'Pic{i}')
+            album.album = photo
+            photo.save()
 
     
     def test_user_is_jimbo(self):
@@ -42,3 +44,27 @@ class PhotoTestCase(TestCase):
         one_user = User.objects.first()
         self.assertEqual(one_user.username, 'Jimbo')
 
+
+    def test_user_has_30_photos(self):
+        """Test that user Jimbo has 30 photo."""
+        one_user = User.objects.first()
+        self.assertEqual(one_user.profile.photo_set.count(), 30)
+
+
+    def test_first_photo_title_is_pic0(self):
+        """Test that user Jimbo has 30 photo."""
+        one_user = User.objects.first()
+        pic = one_user.profile.photo_set.first()
+        self.assertEqual(pic.title, 'Pic0')
+
+
+    def test_album_created(self):
+        """Thest that the album is created."""
+        one_album = Album.objects.get()
+        self.assertIsNotNone(one_album)
+
+
+    def test_album_title_is_the_album(self):
+        """Thest that the album title is The Albumn."""
+        one_album = Album.objects.get()
+        self.assertEqual(one_album.title, 'The Album')
